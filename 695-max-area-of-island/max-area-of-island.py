9773bc8bed4b0d41141b78b9cdd045bb72a0564d
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2025-06-30 21:26:27 -0300

    Time: 24 ms (48.08%) | Memory: 18.3 MB (65.93%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -4,22 +4,18 @@ class Solution:
         maxArea = 0
 
         def dfs(x,y):
-            if not (0 <= x < len(grid) and 0 <= y < len(grid[0])):
+            if not (0 <= x < len(grid) and 0 <= y < len(grid[0])) or not grid[x][y]:
                 return 0
-            
-            if grid[x][y] == 0:
-                return 0
-            
-            area = 1
-            grid[x][y] = 0
-
+                
+            grid[x][y], area = 0, 1
             for m,n in moves:
                 area += dfs(x+m, y+n)
             return area
         
         for x in range(len(grid)):
             for y in range(len(grid[0])):
-                maxArea = max(maxArea, dfs(x,y))
+                if grid[x][y]:
+                    maxArea = max(maxArea, dfs(x,y))
 
         return maxArea
         
