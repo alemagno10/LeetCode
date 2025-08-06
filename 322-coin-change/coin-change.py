87257489767b6d97b335fe65462015b65447e5e9
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2025-08-06 09:22:43 -0300

    Time: 711 ms (77.97%) | Memory: 18 MB (97.26%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,30 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {0:0}
-        coins.sort()
-
-        def search(amount):
-            if amount in memo:
-                return memo[amount]
-            
-            if amount < coins[0]:
-                return float("inf") 
-
-            best = float("inf")           
-            for coin in coins:
-                if coin <= amount:
-                    coins_needed = 0
-                    if amount % coin == 0:
-                        coins_needed = amount // coin
-                    else: 
-                        coins_needed = 1+search(amount-coin)
-                    best = min(best, coins_needed)
-
-            memo[amount] = best
-            return best
+        dp = [1e9]*(amount+1)
+        dp[0] = 0
+        coins = [c for c in coins if c <= amount]
         
-        minCoins = search(amount)
-        return minCoins if minCoins != float("inf") else -1
-                
-            
-                  
+        for coin in coins:
+            dp[coin] = 1
+
+        for i in range(1,amount+1):
+            for coin in coins:
+                if coin < i:
+                    dp[i] = min(dp[i], 1+dp[i-coin])
+
+        return dp[amount] if dp[amount] != 1e9 else -1
