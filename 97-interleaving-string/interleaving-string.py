9b86a50e69e2de4f1fc7ae4a002a03c114c462ed
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2025-08-11 18:29:30 -0300

    Time: 47 ms (47.01%) | Memory: 19 MB (10.99%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -2,23 +2,22 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         memo = {}
         
-        def dfs(i,j,k):
-            if k == len(s3):
+        def dfs(i,j):
+            if i+j == len(s3):
                 return i == len(s1) and j == len(s2)
             
-            key = (i,j,k)
+            key, k = (i,j), i+j
             if key in memo:
                 return memo[key]
             
             res = False
             if i < len(s1) and s1[i] == s3[k]:
-                res |= dfs(i+1, j, k+1)
+                res |= dfs(i+1, j)
 
             if j < len(s2) and s2[j] == s3[k]:
-                res |= dfs(i, j+1, k+1)
+                res |= dfs(i, j+1)
             
             memo[key] = res
             return res
         
-        return dfs(0,0,0)
-
+        return dfs(0,0)
