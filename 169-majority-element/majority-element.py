b4df68154566924a5bf9f2a4d653456e14bbd5be
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2025-08-18 15:13:11 -0300

    Time: 6 ms (60.20%) | Memory: 19.5 MB (18.96%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+
+        for k,v in counter.items():
+            if v >= len(nums)/2:
+                return k
+        return -1
