0f06a22588ffe40ab2625a4fd9a53410095d5226
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2025-09-11 18:47:24 -0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (94.75%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,12 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        letters = defaultdict(int)
-
-        for c in s:
-            letters[c] += 1
+        counter = Counter(s)
 
         res, seenOdd = 0, False
-        for k,v in letters.items():
+        for v in counter.values():
             if v % 2 == 0:
                 res += v
             else:
