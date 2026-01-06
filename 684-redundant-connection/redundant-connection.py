3b667ba7abc11067ebc8d61b86829ba97ff7a15b
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-01-06 15:36:47 +0100

    Time: 0 ms (100.00%) | Memory: 19.7 MB (7.92%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parents = [i for i in range(len(edges)+1)]
+
+        def find(node):
+            """
+            Return the group that a node belongs.
+            """
+            if node != parents[node]:
+                parents[node] = find(parents[node])
+            return parents[node]
+
+        def union(n1, n2):
+            """
+            Unions groups containing n1 and n2.
+            Return whether nodes are connected (have a same parent).
+            """ 
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return False
+            
+            a,b = min(p1,p2), max(p1,p2)
+            parents[a] = b
+            return True
+        
+        for n1, n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+        
