ee955a6f5745212d20197b7bdb5556542eba6604
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-02-10 21:50:04 +0100

    Time: 39 ms (10.09%) | Memory: 20.7 MB (32.46%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,21 +1,21 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        moves = [(0,1),(1,0),(0,-1),(-1,0)]
-        maxArea = 0
+        moves = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        def inBound(x,y):
+            return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
         def dfs(x,y):
-            if not (0 <= x < len(grid) and 0 <= y < len(grid[0])) or not grid[x][y]:
+            if not inBound(x,y) or grid[x][y] == 0:
                 return 0
-                
-            grid[x][y], area = 0, 1
-            for m,n in moves:
-                area += dfs(x+m, y+n)
-            return area
-        
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y]:
-                    maxArea = max(maxArea, dfs(x,y))
 
-        return maxArea
+            res, grid[x][y] = 0,0
+            for dx, dy in moves:
+                res += dfs(x+dx, y+dy)
+            return 1+res
         
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                res = max(res, dfs(i,j))
+        return res
