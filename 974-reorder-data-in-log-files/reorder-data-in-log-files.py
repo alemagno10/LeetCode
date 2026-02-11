c080310bdfcad0dfcc45cf59381c24134d4898ce
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-02-11 21:16:58 +0100

    Time: 0 ms (100.00%) | Memory: 19.5 MB (23.05%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.py
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -4,9 +4,8 @@ class Solution:
         for log in logs:
             ID, val = log.split(" ",1)
             if val[0].isdigit():
-                digit.append(ID+" "+val)
+                digit.append(log)
             else:
-                letter.append((ID,val))
-        res = [x[0]+" "+x[1] for x in sorted(letter, key=lambda x: (x[1],x[0]))]
-        res.extend(digit)
-        return res
+                letter.append((ID,val,log))
+        letter.sort(key=lambda x: (x[1],x[0]))
+        return [x[2] for x in letter] + digit
