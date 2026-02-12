9c4d0cfc3c3a51d68e02eb65714539e18fd61361
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-02-12 12:05:45 +0100

    Time: 189 ms (9.79%) | Memory: 77.4 MB (58.06%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,68 +1,52 @@
 class ListNode:
-    def __init__(self, key=None, value=None, prev=None, nxt=None):
+    def __init__(self, key=None, prev=None, nxt=None):
         self.key = key
-        self.value = value
         self.prev = prev
         self.next = nxt
 
+class LinkedList:
+    def __init__(self, capacity, cache):
+        self.head = ListNode()
+        self.tail = ListNode(prev=self.head)
+        self.head.next = self.tail
+        self.capacity = capacity
+        self.cache = cache
+    
+    def push(self, key):
+        if key in self.cache:
+            self.pop(key)
+        elif len(self.cache) == self.capacity:
+            self.pop()
+
+        node = ListNode(key,self.tail.prev,self.tail)
+        node.prev.next = node
+        self.tail.prev = node
+        self.cache[key] = node
+            
+    def pop(self, key=None):
+        if len(self.cache) == 0:
+            return
+        node = self.cache[key] if key is not None else self.head.next
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        del self.cache[node.key]
+    
 class LRUCache:
     def __init__(self, capacity: int):
         self.cache = {}
-        self.size = 0
-        self.capacity = capacity
-        self.head = None
-        self.tail = None
+        self.map = {}
+        self.list = LinkedList(capacity, self.cache)
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        self.lru(key)
-        return self.cache[key].value
+        self.list.push(key)
+        return self.map[key]
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            if self.size == 0:
-                self.head = ListNode(key=key, value=value)
-                self.tail = self.head
-                self.cache[key] = self.head
-                self.size += 1
-            else:
-                self.head.prev = ListNode(key=key, value=value, nxt=self.head)
-                self.head = self.head.prev
-                self.cache[key] = self.head
-                self.size += 1
+        self.list.push(key)
+        self.map[key] = value        
 
-                if self.size > self.capacity:
-                    tail_prev = self.tail.prev
-                    tail_prev.next = None
-                    self.tail.prev = None
-                    del self.cache[self.tail.key]
-                    self.tail = tail_prev
-                    self.size -= 1
-        else:
-            self.lru(key)
-            self.cache[key].value = value
-
-
-    def lru(self, key):
-        node = self.cache[key]
-        if node == self.head:
-            return 
-
-        if node.prev and node.next:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-
-        elif node == self.tail:
-            self.tail = node.prev
-            self.tail.next = None
-            node.prev = None
-
-        node.next = self.head
-        self.head.prev = node
-        self.head = node
-
-        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
