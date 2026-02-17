e56125db6440fc24be4159b4670f499f761f768f
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-02-17 02:12:38 +0100

    Time: 11 ms (96.90%) | Memory: 42.5 MB (6.32%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 class UndergroundSystem:
     def __init__(self):
-        self.times = defaultdict(list)
+        self.times = defaultdict(lambda: (0,0))
         self.checkins = {}
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
@@ -11,10 +11,12 @@ class UndergroundSystem:
         
     def checkOut(self, id: int, stationB: str, t: int) -> None:
         stationA, timeA = self.checkins[id]
-        self.times[stationA, stationB].append(t-timeA)
+        summ, n = self.times[stationA, stationB] 
+        self.times[stationA, stationB] = (summ+(t-timeA), n+1)
         
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return np.mean(self.times[startStation,endStation])  
+        summ, n = self.times[startStation, endStation]
+        return summ/n
         
 
 
