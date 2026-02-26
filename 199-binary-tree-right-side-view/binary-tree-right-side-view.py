fe0d06eeaf909e398178e9749b53fed623795ce1
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-02-26 10:02:35 -0300

    Time: 1 ms (13.68%) | Memory: 19.3 MB (61.51%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,38 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 from collections import deque
 
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:       
-        if not root:
-            return []
-    
-        queue = deque([(root,0)])
-        mostRight = (root,0)
-        res = []
-        depth = 0
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res, queue = [], deque([root])
 
-        while len(queue) > 0:
-            curr = queue.popleft()
-            if curr[1] > depth:
-                res.append(mostRight[0].val)
-                depth += 1
-            
-            mostRight = curr
+        if root is None:
+            return res
 
-            if curr[0].left:
-                queue.append((curr[0].left, depth+1))
-                        
-            if curr[0].right:
-                queue.append((curr[0].right, depth+1))
-        
-        res.append(mostRight[0].val)
+        while queue:
+            res.append(queue[-1].val)
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+         
         return res
-
-
-        
