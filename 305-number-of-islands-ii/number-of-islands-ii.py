8202ae23743bf7d4247311aa2193069bd8a6229d
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-03-01 15:23:51 -0300

    Time: 71 ms (74.58%) | Memory: 23.5 MB (49.11%) - LeetSync

---
+++ 305-number-of-islands-ii/number-of-islands-ii.py
@@ -0,0 +1,42 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1]*n
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+    
+    def union(self, x1, x2):
+        p1, p2 = self.par[x1], self.par[x2]
+        if p1 == p2:
+            return 0
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return 1
+
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        grid, res, curr = {}, [], 0
+        moves = ((0,1),(0,-1),(1,0),(-1,0))
+        uf = UnionFind(len(positions))
+
+        for i, pos in enumerate(positions):
+            pos = tuple(pos)
+            if pos not in grid:
+                grid[pos] = i
+                curr += 1
+
+                for dx, dy in moves:
+                    step = (pos[0]+dx, pos[1]+dy)
+                    if step in grid:
+                        curr -= uf.union(uf.find(i), uf.find(grid[step]))
+            res.append(curr)
+        return res
+
+
