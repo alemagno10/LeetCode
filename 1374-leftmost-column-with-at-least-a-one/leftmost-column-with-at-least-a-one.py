dbf3c6a575318533866bda0b5d8632ac70614b35
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-03-04 19:40:50 -0300

    Time: 110 ms (5.68%) | Memory: 19.6 MB (78.52%) - LeetSync

---
+++ 1374-leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.py
@@ -0,0 +1,32 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, row: int, col: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        rows, cols = binaryMatrix.dimensions()
+
+        def search(row):
+            l, r = 0, cols-1
+            res = -1
+
+            while l <= r:
+                col = (r+l)//2
+                if binaryMatrix.get(row, col):
+                    r = col-1
+                    res = col
+                else:
+                    l = col+1
+            return res
+
+        res = 1e5
+        for row in range(rows):
+            index = search(row)
+            if index >= 0:
+                res = min(res, index)
+        
+        return res if res != 1e5 else -1
