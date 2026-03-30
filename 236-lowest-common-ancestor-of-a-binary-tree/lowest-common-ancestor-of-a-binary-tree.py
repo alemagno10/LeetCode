618758106fadeb04f5972196e82608869b8db841
Alexandre Magno Maciel dos Santos <104772038+alemagno10@users.noreply.github.com>
2026-03-30 12:16:33 -0300

    Time: 58 ms (70.11%) | Memory: 24.4 MB (65.83%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,25 +7,20 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(root, target, path):
-            if not root:
+        LCA = [root]
+
+        def dfs(node):
+            if not node:
                 return False
+            
+            isTarget = (node == p) or (node == q)
+            left, right = dfs(node.left), dfs(node.right)
+            if int(isTarget) + int(left) + int(right) == 2:
+                LCA[0] = node
 
-            path.append(root)
-            if root == target:
-                return True
+            return isTarget or left or right
 
-            if dfs(root.left, target, path) or dfs(root.right, target, path):
-                return True
-            path.pop()
-            return False
-        
-        p1, p2 = [], []
-        dfs(root, p, p1)
-        dfs(root, q, p2)
+        dfs(root)
+        return LCA[0]
+            
 
-        LCA, i = root, 0
-        for a,b in zip(p1,p2):
-            if a == b:
-                LCA = a
-        return LCA
